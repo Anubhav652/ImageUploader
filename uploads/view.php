a46ab88172b9b6e909bbcf84db28572dbc8a50af
Anubhav652 <anubhavagarwal033@gmail.com>
2015-02-19 20:26:05 +0700

    Version 0.1 build 3 - More more bug fixes

--- uploads/view.php
+++ uploads/view.php
@@ -1,3 +1,6 @@
 <?php
+	echo '<b style="color: yellow;">Author: <i style="color: green;">';
+	echo readfile( $_GET['name'].".info" );
+	echo '</i></b>';
 	echo '<img src="'.$_GET['name'].'" />';
 ?>
