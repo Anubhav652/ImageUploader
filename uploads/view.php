41949bab215192499c9d9e289a9262c7f49911ad
Anubhav652 <anubhavagarwal033@gmail.com>
2015-02-19 20:32:37 +0700

    Version 0.1 build 5 - A clean method
    I made a clean method on this.

--- uploads/view.php
+++ uploads/view.php
@@ -1,6 +1,6 @@
 <?php
 	echo '<b style="color: yellow;">Author: <i style="color: green;">';
-	echo readfile( $_GET['name'].".info" );
-	echo '</i></b>';
+	echo file_get_contents( $_GET['name'].".info" );
+	echo '</i></b><br>';
 	echo '<img src="'.$_GET['name'].'" />';
 ?>
