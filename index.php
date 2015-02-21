fdbce7fb993d959f24f1a963bfedd99a87b5fc11
Anubhav652 <anubhavagarwal033@gmail.com>
2015-02-21 19:10:13 +0800

    Version 0.1 - Build 7: Design Update

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 		<title>
 			Homepage - Images
 		</title>
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  		<script src="js/bootstrap.min.js"></script>
 	</head>
 	<body oncontextmenu="return false;">
 		  <div class="jumbotron">
