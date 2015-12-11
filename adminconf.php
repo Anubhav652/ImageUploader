59b8846a52fcb493323d84725f4b8cd814bc8d8c
Anubhav652 <anubhavagarwal033@gmail.com>
2015-12-11 18:25:32 +0800

    More functions!
    Added more options with comments. DO NOT UPDATE YOUR FILES UNTIL VERSION 2.0 IS RELEASED FOR ALL FILES, IT CAN CAUSE HUGE CONFLICT IN YOUR ADMINISTRATION SYSTEM!

--- adminconf.php
+++ adminconf.php
@@ -1,3 +1,5 @@
 <?php
-	$securekey = "roottext";
+	$username = array( "" ); // Add usernames like this: "Username1", "Username2".etc It can be any username. 
+	$password = array( "" ); // Add passwords like username. Try not to leave it otherwise your system can be hacked easily.
+	$security = true; // Enable password or not
 ?>
