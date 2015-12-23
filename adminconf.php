16140ddaea6417149d595de74725b47500c3f197
Anubhav652 <anubhavagarwal033@gmail.com>
2015-12-23 11:27:02 +0800

    Update adminconf.php

--- adminconf.php
+++ adminconf.php
@@ -1,5 +1,7 @@
-<?php
-	$username = array( "" ); // Add usernames like this: "Username1", "Username2".etc It can be any username. 
-	$password = array( "" ); // Add passwords like username. Try not to leave it otherwise your system can be hacked easily.
-	$security = true; // Enable password or not
+<?php 
+	$username = array( "Hi" ); // Add usernames like this: "Username1", "Username2".etc It can be any username. 
+	$password = array( "Yes" ); // Add passwords like username. Try not to leave it otherwise your system can be hacked easily.
+	$security = true; // Enable password or not (true is recommended for security reasons)
+	$upload = true; // Enable uploading. Change to false if you do not want users to upload anymore.
+	//$comment = true; // Enable commenting or not. Change to false if you do not want users to comment  --- NOt supported for version 2.0
 ?>
